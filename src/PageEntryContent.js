3bd71183235fe262070a19c85125bad08b55bf17
Maciej Wilczyński <mwilczynski@cleeng.com>
2018-09-06 19:14:48 +0200

    Add page support

---
+++ src/PageEntryContent.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./PageEntryContent.css";
+
+class PageEntryContent extends Component {
+	render() {
+		return <div className = "ListEntryRowRight PageEntryContent" >
+				<h1 className="EntryRowRightTitle">
+					{this.props.title}
+				</h1>
+				<div className="EntryRowRightDetails">
+					<span className="EntryRowRightDetailsAuthorDesc">
+						Submitted by: <span className="EntryRowRightDetailsAuthorName">
+							{this.props.author}
+						</span>
+					</span>
+				</div>
+				<div className="PageEntryContentText">
+							<p dangerouslySetInnerHTML={{__html: this.props.body}}>
+							</p>
+				</div>
+			</div>;
+	}
+}
+
+export default PageEntryContent;
---
+++ src/PagePage.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import Header from "./Header.js";
+import { Row, Col, Container } from "reactstrap";
+import "./PagePage.css";
+import PageEntryContent from "./PageEntryContent";
+import LoadingManager from "./LoadingManager";
+import { API_ROOT } from "./ApiConf";
+
+class PagePage extends Component {
+	constructor() {
+		super();
+		this.state = {
+			page: [],
+		};
+	}
+
+	componentDidMount() {
+		LoadingManager.start("PagePage");
+		fetch(`${API_ROOT}/api/page/${this.props.match.params.pageId}`)
+			.then(results => {
+				return results.json();
+			})
+			.then(data => {
+				if (data.status != "OK") {
+					return;
+				}
+				let page = <Row className="PageEntryRow">
+						<div className="PageEntryImage">
+							<div className="PageEntryImage" style={{ backgroundImage: `url('${data.page.meta.photo}')` }} />
+					</div>
+					<Container>
+						<PageEntryContent title={data.page.meta.title} author={data.page.meta.author} body={data.page.content} />
+					</Container>
+					</Row>;
+				this.setState({ page: page });
+				LoadingManager.finish("PagePage");
+			});
+	}
+
+    render() {
+		return <div>
+			<div className="PageEntryShadow" />
+			<div className="Entry">
+				{this.state.page}
+			</div>
+		</div>;
+    
+	}
+}
+
+export default PagePage;

1996e1461aa4238186bcdac47e35b1199112ea0f
Maciej Wilczyński <mwilczynski@cleeng.com>
2018-11-24 12:01:06 +0100

    Fix default locale value

--- src/LocaleManager.js
+++ src/LocaleManager.js
@@ -7,7 +7,8 @@ export default class LocaleManager {
 
 	static getLocale() {
 		if (localStorage.getItem("it_locale") === undefined
-			|| localStorage.getItem("it_locale") === "") {
+			|| localStorage.getItem("it_locale") === ""
+			|| localStorage.getItem("it_locale") === null) {
 			this.setLocale(this.getDefaultLocale())
 		}
 		return localStorage.getItem("it_locale");
@@ -28,7 +29,7 @@ export default class LocaleManager {
 		if (ConfigManager.get() !== undefined) {
 			return ConfigManager.get().site.languages.default;
 		}
-		return "";
+		return "en";
 	}
 
 	static getLocaleNameFromCode(code) {
